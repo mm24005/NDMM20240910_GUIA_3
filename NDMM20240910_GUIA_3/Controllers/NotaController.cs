941b4d86d5542b990476bf4092b97fd18bd964a0
mm24005 <mm24005@esfe.agape.edu.sv>
2024-09-11 14:49:34 -0600

    Nota Controller

---
+++ NDMM20240910_GUIA_3/Controllers/NotaController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NDMM20240910_GUIA_3.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotaController : ControllerBase
+    {
+        private static List<Nota> notas = new List<Nota>();
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IEnumerable<Nota> ObtenerNotas()
+        {
+            // Devuelve todas las notas, ya que este endpoint es público
+            return notas;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public IActionResult RegistrarNota(Nota nota)
+        {
+            // Solo usuarios autenticados pueden registrar notas
+            notas.Add(nota);
+            return CreatedAtAction("ObtenerNota", new { id = notas.Count - 1 }, nota);
+        }
+
+        public class Nota
+        {
+            public int Id { get; set; }
+            public string? Titulo { get; set; }
+            public string? Contenido { get; set; }
+        }
+    }
+}
